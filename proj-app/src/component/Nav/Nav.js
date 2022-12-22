1e39e54d05f3c1748b3b425e118cffe033fda040
unknown <adityasailella@gmail.com>
2022-12-22 23:37:46 +0530

    Nav

---
+++ proj-app/src/component/Nav/Nav.js
@@ -0,0 +1,15 @@
+import './nav.css'
+import { useState,useEffect } from 'react';
+
+
+const Nav =() =>{
+    useEffect(()=>{},[]);
+    
+    
+    return(<section>
+        <div className="nav_left">
+            <img className='logo' src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"></img>            
+        </div>
+    </section>);
+}
+export default Nav;
