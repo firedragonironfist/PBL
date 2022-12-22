77986ceee12d4dd4fde26bf9101c7dfaa41adc17
Yousuf <firedragonironfist998@gmail.com>
2022-12-22 23:57:52 +0530

    1

--- proj-app/src/App.js
+++ proj-app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './App.css';
+import Home from './pages/Home.css'
 
 function App() {
   return (
     <div className="App">
       <h1>Our PBL Project</h1>
+      <Home />
     </div>
   );
 }
--- proj-app/src/pages/Home.js
+++ proj-app/src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import "./App.css0"
 import "./Home.css";
 
 function Home(){
     return(
         <div className='home'>
-
+            <h1>This is Homepage</h1>
         </div>
     )
 }
 
-export default Home
+export default Home;
