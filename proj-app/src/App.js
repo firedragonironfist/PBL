b3830bbbdca35b52912dd3e27ff89b94dd55ba92
Yousuf <firedragonironfist998@gmail.com>
2022-12-23 12:39:52 +0530

    5

--- proj-app/src/App.js
+++ proj-app/src/App.js
@@ -11,7 +11,37 @@ function App() {
     <div class="dropdown">
       <button class="link">Area</button>
       <div class="dropdown-menu">
-        Dropdown content
+        <div class="dropdown-links">
+        <a href="proj-app\src\component\page.html">Andhra Pradesh</a>
+        <a href="proj-app\src\component\page.html">Arunachal Pradesh</a>
+        <a>Assam</a>
+        <a>Bihar</a>
+        <a>Chhattisgarh</a>
+        <a>Goa</a>
+        <a>Gujarat</a>
+        <a>Haryana</a>
+        <a>Himachal Pradesh</a>
+        <a>Jharkhand</a>
+        <a>Karnataka</a>
+        <a>Kerala</a>
+        <a>Madhya Pradesh</a>
+        <a>Maharashtra</a>
+        <a>Manipur</a>
+        <a>Meghalaya</a>
+        <a>Mizoram</a>
+        <a>Nagaland</a>
+        <a>Odisha</a>
+        <a>Punjab</a>
+        <a>Rajasthan</a>
+        <a>Sikkim</a>
+        <a>Tamil Nadu</a>
+        <a>Telangana</a>
+        <a>Tripura</a>
+        <a>Uttar Pradesh</a>
+        <a>Uttarakhand</a>
+        <a>West Bengal</a>
+
+        </div>
       </div>
     </div>
     </body>
